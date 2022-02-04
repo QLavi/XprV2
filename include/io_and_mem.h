8c18f292804d983cf307bed4a90bfe80e7bf160a
QLavi <LavenRose@protonmail.com>
2022-02-05 01:18:38 +0530

    Added debugging features to x_alloc

--- include/io_and_mem.h
+++ include/io_and_mem.h
@@ -3,10 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ALLOC(type, count) (type*)x_alloc(NULL, sizeof(type) * (count))
-#define REALLOC(type, old_ptr, count)  (type*)x_alloc(old_ptr, sizeof(type) * (count))
-#define FREE(type, ptr) x_alloc(ptr, 0)
+#define ALLOC(type, count) (type*)x_alloc(NULL, sizeof(type) * (count), __LINE__, __FILE__)
+#define REALLOC(type, old_ptr, count)  (type*)x_alloc(old_ptr, sizeof(type) * (count), __LINE__, __FILE__)
+#define FREE(type, ptr) x_alloc(ptr, 0, __LINE__, __FILE__)
 
-void* x_alloc(void* old_ptr, size_t size);
+void* x_alloc(size_t* old_ptr, size_t size, int line_no, char* filename);
 char* load_file(char* filename);
 
--- src/io_and_memory.c
+++ src/io_and_memory.c
@@ -1,16 +1,37 @@
 #include "io_and_mem.h"
 
-void* x_alloc(void* old_ptr, size_t size) {
+/* #define DEBUG_ALLOC */
+
+void* x_alloc(size_t* old_ptr, size_t size, int line_no, char* filename) {
     if(old_ptr != NULL && size == 0) {
+
+#ifdef DEBUG_ALLOC
+        printf("%s:%i: free: %zu\n", filename, line_no, old_ptr[-1]);
+        free(&old_ptr[-1]);
+#else
         free(old_ptr);
+#endif
         return NULL;
     }
-    void* new_ptr = realloc(old_ptr, size);
-    if(new_ptr == NULL) {
-        fprintf(stderr, "Out_Of_Memory_Error!\nAborted");
-        exit(1);
+
+    if(size != 0) {
+#ifdef DEBUG_ALLOC
+        size_t* new_ptr = realloc(old_ptr, sizeof(size_t) + size);
+        *new_ptr = size;
+        printf("%s:%i: alloc: %zu\n", filename, line_no, size);
+#else
+        void* new_ptr = realloc(old_ptr, size);
+#endif
+
+        if(new_ptr == NULL) {
+            fprintf(stderr, "Out_Of_Memory_Error!\nAborted");
+            exit(1);
+        }
+#ifdef DEBUG_ALLOC
+        return &new_ptr[1];
+#endif
+        return new_ptr;
     }
-    return new_ptr;
 }
 
 char* load_file(char* filename) {
