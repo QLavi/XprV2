e37fc87cb9f43cd3cddb656411da9e8fad334190
QLavi <LavenRose@protonmail.com>
2022-02-06 23:59:18 +0530

    Refactored on run_tests script for running multiple tests

--- run_tests.py
+++ run_tests.py
@@ -4,23 +4,27 @@ def load_test_file(filename):
     with open(filename, "r") as f:
         return f.readlines()
 
-values = []
-for line in load_test_file("tests/expression.xpr"):
-    values.append(re.findall("Expect: -?[\d\.\d]*", line))
+def run_test(test_file):
+    values = []
+    for line in load_test_file(test_file):
+        values.append(re.findall("Expect: -?[\d\.\d]*", line))
 
-values = values[1:]
-values = [x[0].split(':') for x in values if x != []]
-values = [float(x[1]) for x in values]
+    values = values[1:]
+    values = [x[0].split(':') for x in values if x != []]
+    values = [float(x[1]) for x in values]
+
+    capture = subproc.run(["./xpr", test_file], capture_output=True)
+    got = str(capture.stdout)
+    got = re.findall("-?[\d\.\d]*", got)
+    got = [float(x) for x in got if x != '']
+
+    result = [x == y for x, y in zip(values, got)]
+    if False not in result:
+        print("[RESULT] === PASSED ===")
+    else:
+        print("[RESULT] === FAILED ===")
+
 
 print("Running Tests")
 print(f"[TEST] Expression Parsing and Evaluation")
-capture = subproc.run(["./xpr", "tests/expression.xpr"], capture_output=True)
-got = str(capture.stdout)
-got = re.findall("-?[\d\.\d]*", got)
-got = [float(x) for x in got if x != '']
-result = [x == y for x, y in zip(values, got)]
-if False in result:
-    print("[RESULT] === Failed ===")
-else:
-    print("[RESULT] === Passed ===")
-
+run_test("tests/expression.xpr")
